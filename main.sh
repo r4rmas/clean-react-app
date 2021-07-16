a029781b050e913d88dadc11a4082afbdab1a18a
Raul Armas <ra@raularmas.com>
2021-07-15 22:42:43 -0500

    ...

--- main.sh
+++ main.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-npx create-react-app $1
-rm -rf $(pwd)/$1/public
-rm -rf $(pwd)/$1/src
-rm -f $(pwd)/$1/README.md
-cp -r ~/.Scripts/clean-react-app/public $(pwd)/$1/
-cp -r ~/.Scripts/clean-react-app/src $(pwd)/$1/
-cp -r ~/.Scripts/clean-react-app/.vscode $(pwd)/$1/
-cd $(pwd)/$1/
+cp -r ~/.Scripts/clean-react-app/app/. $(pwd)
